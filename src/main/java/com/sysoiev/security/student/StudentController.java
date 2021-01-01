eb759ee521cd07c15c523cf7bab492db4a3ba31e
Bada1208 <sysoievbogdan@gmail.com>
2021-01-01 14:02:53 +0100

    add student & controller

---
+++ src/main/java/com/sysoiev/security/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.sysoiev.security.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Galia"),
+            new Student(2, "Ania"),
+            new Student(3, "Vania")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student " + studentId + " does not exists"));
+    }
+}
