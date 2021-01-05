d3d5feef7fc522200370f7e6c2dc0445ab587182
Bada1208 <sysoievbogdan@gmail.com>
2021-01-05 06:24:42 +0100

    add logout code to config

--- src/main/java/com/sysoiev/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sysoiev/security/security/ApplicationSecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.sysoiev.security.security.ApplicationUserPermission.*;
 import static com.sysoiev.security.security.ApplicationUserRole.*;
@@ -51,8 +54,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();//defaults to 2 weeks
-
+                //.rememberMe();//defaults to 2 weeks
+                .rememberMe()
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
